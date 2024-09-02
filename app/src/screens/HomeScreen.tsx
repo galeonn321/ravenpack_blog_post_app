918f052383db07539a40e9cbf137488908931cd2
galeonn321 <santichiste@hotmail.com>
2024-09-02 23:32:35 +0200

    update last component

--- app/src/screens/HomeScreen.tsx
+++ app/src/screens/HomeScreen.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Box, Image, Text, View } from '@gluestack-ui/themed';
 import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
-import { FlatList } from 'react-native-gesture-handler';
-import PostCard from '../components/PostCard';
-import { LOG } from '../config/logger';
-import { getPosts } from '../actions';
-import { ActivityIndicator, Dimensions } from 'react-native';
 import { FlashList } from '@shopify/flash-list';
+import PostCard from '../components/PostCard';
+import { getPosts } from '../actions';
+import { ActivityIndicator, Dimensions, StyleSheet } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+
+interface UserList {
+  name: string;
+  id: number;
+}
 
 const { width, height } = Dimensions.get('window');
 
 const HomeScreen = () => {
   const queryClient = useQueryClient();
 
+  // Select user
+  const [selectedUserId, setSelectedUserId] = useState<number>(0);
+  const [users, setUsers] = useState<UserList[]>([{ name: 'all', id: 0 }]);
+
   const { data, isLoading, fetchNextPage } = useInfiniteQuery({
-    queryKey: ['posts', 'infinite'],
+    queryKey: ['posts', 'all'],
     initialPageParam: 0,
     staleTime: 1000 * 60 * 60,
     queryFn: async () => {
@@ -29,28 +37,75 @@ const HomeScreen = () => {
     getNextPageParam: (pages) => pages.length,
   });
 
+  const allPosts = data?.pages.flat() ?? [];
+
+  useEffect(() => {
+    const userMap = new Map<number, UserList>();
+    allPosts.forEach((post) => {
+      if (!userMap.has(post.user.id)) {
+        userMap.set(post.user.id, post.user);
+      }
+    });
+
+    const newUsers = [{ name: 'all', id: 0 }, ...Array.from(userMap.values())];
+    setUsers((prevUsers) => {
+      if (JSON.stringify(prevUsers) !== JSON.stringify(newUsers)) {
+        return newUsers;
+      }
+      return prevUsers;
+    });
+  }, [allPosts]);
+
+  // Filter posts by selected user
+  const filteredPosts = selectedUserId
+    ? allPosts.filter((post) => post.user.id === selectedUserId)
+    : allPosts;
+
+  // Find the selected user object
+  const selectedUser = useMemo(
+    () => users.find((user) => user.id === selectedUserId),
+    [users, selectedUserId]
+  );
+
   return (
     <View flex={1} bgColor='#FFF'>
       {isLoading ? (
         <ActivityIndicator size={'large'} color={'#D8A25E'} />
       ) : (
-        <Box width={width} height={height}>
+        <Box width={width} height={height} mb='$48'>
           <FlashList
-            data={data?.pages.flat() ?? []}
+            data={filteredPosts}
             keyExtractor={(post, index) => `${post}-${index}`}
             numColumns={1}
             renderItem={({ item }) => <PostCard post={item} />}
             estimatedItemSize={width * 0.9}
-            onEndReached={() => fetchNextPage()}
             onEndReachedThreshold={0.6}
+            contentContainerStyle={{ paddingBottom: 100 }}
             showsVerticalScrollIndicator={false}
             ListHeaderComponent={() => (
-              <Image
-                source={require('../../../assets/images/icon_logo_top.png')}
-                alt='logo top image'
-                size='lg'
-                alignSelf='center'
-              />
+              <Box mx='$4'>
+                <Image
+                  source={require('../../../assets/images/icon_logo_top.png')}
+                  alt='logo top image'
+                  size='lg'
+                  alignSelf='center'
+                />
+                <Dropdown
+                  style={[styles.dropdown]}
+                  placeholderStyle={styles.placeholderStyle}
+                  selectedTextStyle={styles.selectedTextStyle}
+                  itemTextStyle={{ color: 'gray' }}
+                  data={users}
+                  maxHeight={300}
+                  labelField='name'
+                  valueField='id'
+                  placeholder='Select a user'
+                  value={selectedUser}
+                  onChange={(item: UserList) => {
+                    setSelectedUserId(item.id);
+                  }}
+                />
+              </Box>
             )}
           />
         </Box>
@@ -60,6 +115,35 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+  },
+
+  dropdown: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+
+  placeholderStyle: {
+    fontSize: 16,
+    color: 'gray',
+  },
+
+  selectedTextStyle: {
+    fontSize: 16,
+    color: 'gray',
+  },
+});
 
 {
   /* <FlatList
